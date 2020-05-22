e42b3644439aaa75ea7890249ada6432df6911df
Bartosz Telenczuk <bartosz.telenczuk@gmail.com>
2020-05-22 13:11:14 +0200

    updated for new release

--- setup.py
+++ setup.py
@@ -5,11 +5,12 @@ with open("README.md", "r") as fh:
 
 setup(
     name="example_magic",
-    version="0.1",
+    version="0.1.2",
     author="Bartosz Telenczuk and IPython Development Team",
     author_email="bartosz@telenczuk.pl",
     description="Example IPython magic extension (used for testing)",
     long_description=long_description,
+    long_description_content_type='text/markdown',
     url="https://github.com/btel/example_magic",
     packages=find_packages(),
     classifiers=[
